881bb7009e26487a6d808224d2db035db8d49827
rafaelvalquer <109303318+rafaelvalquer@users.noreply.github.com>
2026-09-06 21:57:07 -0300

    test: expose Room schemas to migration tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,10 @@ android {
         buildConfig = true
     }
 
+    sourceSets {
+        getByName("androidTest").assets.srcDir("$projectDir/schemas")
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
